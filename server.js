3895432b228ad451d5b181909128937413fff604
whatgum <whatgum@gmail.com>
2021-02-11 16:43:34 -0500

    created generic router

--- server.js
+++ server.js
@@ -12,15 +12,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(expressjson());
 
+//get uri for database
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParse: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
 
+//connect to database
 connection.once("open", () =>
 {
     console.log("mongoDB database conneciton established successfullly")
 })
+
+app.use("/generic", genericRouter);
 
 //start the server
 app.listen(port,() =>
