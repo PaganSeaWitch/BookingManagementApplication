030e2894aebccb684254c52f914c8d0ab21bc6ba
whatgum <whatgum@gmail.com>
2021-02-11 16:55:55 -0500

    added generic router

--- Back-end/Routes/genericRouter.js
+++ Back-end/Routes/genericRouter.js
@@ -1,1 +1,21 @@
-// JavaScript source code
+const router = require('express').Router();
+let Generic = require("../Models/generic.model");
+
+router.route("/").get((req, res) => {
+    Generic.find()
+        .then(Generics => res.json(generics))
+        .catch(err => res.status(400).json("Error: " + err))
+});
+
+router.route("/add").post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newGeneric = new Generic({ username, password });
+
+    newGeneric.save()
+        .then(() => res.json("Generic added!"))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
