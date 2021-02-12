c57a74c73833d04d42c4ebe961bd5b8f09143380
whatgum <whatgum@gmail.com>
2021-02-11 23:38:18 -0500

    finished generic back end, starting on generic front end

--- Back-end/Routes/generics.js
+++ Back-end/Routes/generics.js
@@ -18,4 +18,29 @@ router.route("/add").post((req, res) => {
         .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+    Generic.findById(req.params.id)
+        .then(generic => res.json(generic))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+    Generic.findByIdAndDelete(req.params.id)
+        .then(() => res.json("Generic deleted."))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+    Generic.findByIdAndUpdate(req.params.id)
+        .then((generic) => {
+            generic.username = req.body.username;
+            generic.password = req.body.password;
+
+            generic.save()
+                .then(() => res.json("Generic updated."))
+                .catch(err => res.status(400).json("Error: " + err));
+        })
+        .catch(err => res.status(400).json("Error: " + err))
+})  
+
 module.exports = router;
