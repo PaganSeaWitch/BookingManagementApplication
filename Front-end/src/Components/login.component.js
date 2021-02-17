6eeab4ddf084766743b089e3c302438a70bfc368
whatgum <whatgum@gmail.com>
2021-02-17 00:00:53 -0500

    nearly finished CSS for login

--- Front-end/src/Components/login.component.js
+++ Front-end/src/Components/login.component.js
@@ -1,29 +1,52 @@
 import Button from '@material-ui/core/Button';
 import { useState, useEffect } from "react";
-
 const Login = (getUser) => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     return (
         <div>
+            <div className={"login-header"}>
+                <header> Hotels R US Login
+                    <div className={"bottom-right-corner"}>
+                        <Button> User </Button>
+                        <Button> Manager </Button>
+                    </div>
+                </header>
+            </div>
             <form className={"login-form"}>
-                <header className={"main-header"}> Hotels R US Login</header>
                 <div>
-                    <label>Username:</label>
-                    <input
+                    <span></span>
+                        <label>Username</label>
+                    <span></span>
+                </div>
+                
+                <div>
+                    <span></span>
+
+                    <input className={"rounded-login"}
                         type='text'
                         value={username}
                         onChange={(e) => {setUsername(e.target.value); }
                         } />
+                    <span></span>
+
                 </div>
+                <br></br>
                 <div>
-                    <label>Password:</label>
-                    <input
+                    <span></span>
+                    <label>Password</label>
+                    <span></span>
+                </div>
+
+                <div>
+                    <span></span>
+                    <input className={"rounded-login"}
                         type={'password'}
                         value={password}
                         onChange={(e) => {  setPassword(e.target.value); }
                         } />
+                    <span></span>
                 </div>
             </form>
         </div>
