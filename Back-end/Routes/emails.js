85b7c816bccfc8609b4470d0ada4ed3290a06a07
whatgum <palmerjw@iu.edu>
2021-03-07 12:44:56 -0500

    added the accountRecovery add and delete All

--- Back-end/Routes/emails.js
+++ Back-end/Routes/emails.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const validator = require("email-validator");
+let AccountRecovery = require("../Models/account-recovery.model");
+
+
 router.route("/checkEmail/:email").get((req, res) => {
     console.log(req.params.email)
     const email = req.params.email;
@@ -10,6 +13,25 @@ router.route("/checkEmail/:email").get((req, res) => {
     if (check == false) {
         res.json("no")
     }
+});
+
+router.route("/AccountRecovery/Add").post((req, res) => {
+    const email = req.params.email;
+    const account_id = req.params.account_id;
+    const accountType = req.params.accountType;
+    const newAccountRecovery = new AccountRecovery({ email, account_id, accountType });
+    console.log(newAccountRecovery);
+    newAccountRecovery.save()
+        .then(() => res.json(newAccountRecovery))
+        .catch(err => res.status(400).json("Error: " + err));
+
+});
+
+router.route("/AccountRecovery/DeleteAllByEmail/:email").delete((req, res) => {
+    AccountRecovery.deleteMany({ email: req.params.email })
+        .then(() => res.json("accountRecoveryRequests deleted."))
+        .catch(err => res.status(400).json("Error: " + err));
+
 });
 
 module.exports = router;
