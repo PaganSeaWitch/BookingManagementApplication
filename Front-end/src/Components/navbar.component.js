50a53132d53af68cb650f25d45db81efcd8e24c8
whatgum <palmerjw@iu.edu>
2021-03-09 13:25:17 -0500

    fixed navbar

--- Front-end/src/Components/navbar.component.js
+++ Front-end/src/Components/navbar.component.js
@@ -9,43 +9,51 @@ const NavBar = ({  user, manager }) => {
     <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
             {/* This navbar based off the bootstrap navbar */}
         <Link to="" className="navbar-brand">RendezView</Link>
-        <div className="collapse navbar-collapse" >
-                <ul className="navbar-nav mr-auto">
-                    {/* creates the items in the navbar for navigation */}
-                    {(user._id == "" && manager._id == "") //if true, display first div
-                        ? <div>
+            <div className="collapse navbar-collapse" >
+                {(user._id == "" && manager._id == "") //if true, display first div
+                    ? <div>
+                        <ul className="navbar-nav mr-auto">
+                            {/* creates the items in the navbar for navigation */}
                             <li className="navbar-item">
                                 <Link to="/create" className="nav-link">New Account</Link>
-                                </li>
-                            
+                            </li>
+
                             <li className="navbar-item">
                                 <Link to="/Login" className="nav-link">Returning Account</Link>
-                                </li>
+                            </li>
 
-							<li className="navbar-item">
+                            <li className="navbar-item">
                                 <Link to="/dashboard" className="nav-link">Dashboard</Link>
-							</li>
+                            </li>
+                    
+                        </ul>
+                        
+                            
                         </div>
-                        : <div>{(manager._id != "") //If not true, display second div
-                            ? <div>
+                    : <div>{(manager._id != "") //If not true, display second div
+                        
+                        ? <div>
+                            < ul className="navbar-nav mr-auto">
                                 <li className="navbar-item">
                                     <Link to="/manager" className="nav-link">Account Information</Link>
                                 </li>
 								<li className="navbar-item">
                                 <Link to="/dashboard" className="nav-link">Dashboard</Link>
-								</li>
-                            </div>
-                            :
-                            <div>
+                                </li>
+                            </ul>
+                        </div>
+                        : <div>
+                            < ul className="navbar-nav mr-auto">
                                 <li className="navbar-item">
                                     <Link to="/user" className="nav-link">account Information</Link>
                                 </li>
 								<li className="navbar-item">
                                 <Link to="/dashboard" className="nav-link">Dashboard</Link>
-								</li>
+                                </li>
+                            </ul>
                             </div>}
                         </div>}
-            </ul>
+           
         </div>
     </nav>);
 };
