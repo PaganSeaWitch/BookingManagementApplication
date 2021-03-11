1ade5f05b8c0bd6fb965593ca30184b3eb264916
whatgum <palmerjw@iu.edu>
2021-03-11 17:19:17 -0500

    not working rn

--- Front-end/src/Components/bookings.component.js
+++ Front-end/src/Components/bookings.component.js
@@ -1,1 +1,32 @@
 // JavaScript source code
+const Bookings = ({ user, getHotel, getRoom, onRoomClick, props }) => {
+    const [hotelName, setHotelName] = useState("");
+    const [hotelCity, setHotelCity] = useState("");
+    const [hotelCountry, setHotelCountry] = useState("");
+
+
+    useEffect(() => {
+
+        
+        
+
+
+    }, []);
+
+
+    return (
+        <div>
+
+            <header>
+                Current Bookings for {user.username}
+            </header>
+
+
+            <ul style={{ listStyleType: "none" }}>
+                {user.bookings.map((booking, index) => { <BookingListing key={index} hotel={getHotel(booking.hotel_ID)} room={getRoom(booking.room_ID)} onClick={onRoomClick} props={props} /> })}
+            </ul>
+        </div>
+    )
+}
+
+export default Bookings
