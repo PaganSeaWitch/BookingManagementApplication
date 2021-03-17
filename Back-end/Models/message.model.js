0a43128b5d22f54e6729aa4ce34a6fe382697153
whatgum <palmerjw@iu.edu>
2021-03-17 12:13:00 -0400

    created message model, fixed backend stuff

---
+++ Back-end/Models/message.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema
+    ({
+        subject:
+        {
+            type: String,
+            required: true
+        },
+        body:
+        {
+            type: String,
+            required: true
+        },
+        recipient:
+        {
+            type: String,
+            required: true
+        },
+        sender:
+        {
+            type: String,
+            required: true
+        },
+        recipient_id:
+        {
+            type: String,
+            required: true
+        },
+        sender_id:
+        {
+            type: String,
+            required: true
+        }
+    })
+
+const Message = mongoose.model("Message", messageSchema);
+
+
+module.exports = Message 
