5c27ea031dd9104e6914e8a99fc25384419787a0
palmerjw <palmerjw@iu.edu>
2021-03-23 23:44:45 +0000

    added keys and such

--- Back-end/server.js
+++ Back-end/server.js
@@ -7,6 +7,8 @@ const emailRouter = require("./Routes/emails")
 const hotelRouter = require("./Routes/hotels")
 const roomRouter = require("./Routes/rooms")
 const messageRouter = require("./Routes/messages")
+const fs = require("fs");
+const https = require("https");
 require('dotenv').config();
 
 
@@ -15,31 +17,41 @@ require('dotenv').config();
 //for instance, the url to the mongoDB is the ATLAS_URI
 
 const app = express();
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
+console.log(port);
 
 
 app.use(cors());
 app.use(express.json());
 
+
 //get uri for database
 const uri = process.env.ATLAS_URI;
+console.log(uri);
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 const connection = mongoose.connection;
-
-//connect to database
-connection.once("open", () => {
-    console.log("mongoDB database conneciton established successfullly")
-});
 
 app.use("/user", userRouter)
 app.use("/manager", managerRouter)
 app.use("/email", emailRouter)
 app.use("/hotel", hotelRouter)
 app.use("/room", roomRouter)
-app.use("/message", messageRouter)
-//start the server
-app.listen(port, () => {
-    console.log(`server is running on port: ${port}`);
+app.use("message", messageRouter)
+
+//connect to database
+connection.once("open", () => {
+    console.log("mongoDB database conneciton established successfullly")
 });
+
+
+
+https.createServer({
+	key: fs.readFileSync("server.key"),
+	cert: fs.readFileSync("server.cert")
+},app)
+	.listen(port, function(){
+		console.log("listeing on port 5000!")
+	})
+
