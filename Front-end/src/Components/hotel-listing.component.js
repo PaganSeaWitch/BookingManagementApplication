a378c4b1cc1ab89223873d7bbacd99315fd71145
whatgum <palmerjw@iu.edu>
2021-03-26 18:53:14 -0400

    hotel

--- Front-end/src/Components/hotel-listing.component.js
+++ Front-end/src/Components/hotel-listing.component.js
@@ -1,23 +1,26 @@
-import { BiArrowBack } from 'react-icons/bi'
+import { FaHotel } from 'react-icons/fa'
 
 const HotelListing = ({ hotel, onClick, props }) => {
     return (
-        <div className = 'hotel-listing'>
+        <div className={'hotel-listing'} onClick={() => onClick(hotel._id, props)} >
 			
-            <h3>
-                {hotel.name}{' '}
-                {/* creates a x icon */}
-                <BiArrowBack
-                    style={{ color: 'green', cursor: 'pointer' }}
+            <heading >
+                <h2 className={"hotel-header"}><FaHotel
+                    style={{ color: 'black'}}
                     onClick={() => onClick(hotel._id, props)}
-                />
-
-            </h3>
-            <img className = 'hotel-image' src = {'https://cache.marriott.com/marriottassets/marriott/RDURN/rdurn-exterior-0048-hor-feat.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1180px:*'}/> 
-            <div>
-                <label className={"hotel-left-label"}>City : {hotel.location.city}       </label>
-                <label className={"hotel-right-label"}> State : {hotel.location.stateOrProvince} </label>
-			</div>
+                />{" "}{hotel.name}{' '}
+                {/* creates a x icon */}
+                    
+                        
+                        <div>
+                            <label className={"hotel-left-label"}> State : {hotel.location.stateOrProvince}      </label>
+                            <label className={"hotel-right-label"}>  City : {hotel.location.city} </label>
+                        </div>
+                </h2>
+                
+            </heading>
+            <img className={'hotel-image'} src = {'https://cache.marriott.com/marriottassets/marriott/RDURN/rdurn-exterior-0048-hor-feat.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1180px:*'}/> 
+            
         </div>
     )
 }
