2c2ffd51917433f908394f83fdebf503911e3867
whatgum <palmerjw@iu.edu>
2021-04-20 13:01:16 -0400

    prelminiary

---
+++ Front-end/src/Components/manager-statistics.component.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+const ManagerStats = ({ manager }) => {
+
+    return (
+
+        <div>
+            <p>This is the manager dashboard</p>
+        </div>
+
+    );
+}
+
+export default ManagerStats
