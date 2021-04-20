3b1271b03660a6594fe9e1e3b583a4edb612762a
whatgum <palmerjw@iu.edu>
2021-04-20 12:49:58 -0400

    Update splashPage.component.js

--- Front-end/src/Components/splashPage.component.js
+++ Front-end/src/Components/splashPage.component.js
@@ -8,9 +8,6 @@ const SplashPage = ({user, manager, cities, onCityClick, props }) => {
 	return(
 			 <div className = 'splash-page'>
 				<center>	
-				<span className = 'splash'> 
-					  <Link to={"/Dashboard"} >See our bookings!</Link>
-				</span>
 				</center>
 				<ul style={{ listStyleType: "none" }}>
 					{cities.map((newCity) => <City city={newCity} onClick={onCityClick} props={props} />)}
